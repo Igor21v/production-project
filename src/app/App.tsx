d534a98d4c6fe7276c60e9a1647945aff6c403b4
igor21v <igor21v@mail.ru>
2022-09-23 16:41:28 +0800

    12 -1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,16 @@
-import React, { Suspense } from 'react'
-import { Link } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
 import './styles/index.scss'
 import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme'
 import { classNames } from 'shared/lib/classNames/classNames'
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
 import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 export default function App() {
   const {theme, toggleTheme} = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
+      <Navbar/>
       <AppRouter/>
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   )
 }
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type Mode = Record<string, string| boolean>
 
-export function classNames( cls: string, mode: Mode, additional: string[]): string {
+export function classNames( cls: string, mode: Mode = {}, additional: string[] = []): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         ...Object.entries(mode)
             .filter(([className, value]) => Boolean(value))
             .map(([className]) => className)
