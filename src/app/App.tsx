0aa47c7f5025c74f3452c99f868f7eb74bb8b20c
Igor Bondarenko <igor21v@mail.ru>
2022-10-16 09:16:26 +0800

    22 start

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,16 @@ import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { AppRouter } from './providers/router';
 
 export default function App() {
     const { theme } = useTheme();
-
+    useEffect(() => {
+        if (Math.random() < 0.5) {
+            throw new Error();
+        }
+    }, []);
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
