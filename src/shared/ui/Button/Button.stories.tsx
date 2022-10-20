df6883acf1426f0d27841f4d3469c4755f5d161e
igor21v <igor21v@mail.ru>
2022-10-20 16:59:51 +0800

    add settings to vscode for checking null

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,4 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Button, ThemeButton } from './Button';
 
 export default {
@@ -16,8 +18,34 @@ Primary.args = {
     children: 'Text',
 };
 
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+    children: 'Text',
+};
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
+
 export const Clear = Template.bind({});
 Clear.args = {
     children: 'Text',
     theme: ThemeButton.CLEAR,
 };
+
+export const ClearDark = Template.bind({});
+ClearDark.args = {
+    children: 'Text',
+    theme: ThemeButton.CLEAR,
+};
+ClearDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Outline = Template.bind({});
+Outline.args = {
+    children: 'Text',
+    theme: ThemeButton.OUTLINE,
+};
+
+export const OutlineDark = Template.bind({});
+OutlineDark.args = {
+    children: 'Text',
+    theme: ThemeButton.OUTLINE,
+};
+OutlineDark.decorators = [ThemeDecorator(Theme.DARK)];
