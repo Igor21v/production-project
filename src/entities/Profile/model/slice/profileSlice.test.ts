19a321c76115eab61c8029cc9d7e0378b761f51a
igor21v <igor21v@mail.ru>
2022-12-01 11:55:37 +0300

    Added all tests for profile component

--- src/entities/Profile/model/slice/profileSlice.test.ts
+++ src/entities/Profile/model/slice/profileSlice.test.ts
@@ -1,13 +1,69 @@
-import { LoginSchema } from '../types/loginSchema';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import { updateProfileData } from '../services/updateProfileData/updateProfileData';
+import { ProfileSchema, ValidateProfileError } from '../types/profile';
 import { profileActions, profileReducer } from './profileSlice';
 
-describe('loginSlice.test', () => {
+const data = {
+    first: 'Asd',
+    age: 23,
+    city: 'Test',
+    country: Country.Belarus,
+    lastname: 'Flcvb',
+    currency: Currency.EUR,
+    username: 'hhh',
+};
+
+describe('profileSlice.test', () => {
     test('setUsername', () => {
-        const state: DeepPartial<LoginSchema> = { username: '123' };
-        expect(profileReducer(state as LoginSchema, loginActions.setUsername('456rty'))).toEqual({ username: '456rty' });
+        const state: DeepPartial<ProfileSchema> = { readonly: false };
+        expect(profileReducer(
+            state as ProfileSchema,
+            profileActions.setReadonly(true),
+        )).toEqual({ readonly: true });
     });
-    test('setPassword', () => {
-        const state: DeepPartial<LoginSchema> = { password: '123' };
-        expect(profileReducer(state as LoginSchema, loginActions.setPassword('456rty'))).toEqual({ password: '456rty' });
+    test('cancelEdit', () => {
+        const state: DeepPartial<ProfileSchema> = { readonly: false, validateErrors: [ValidateProfileError.INCORRECT_AGE], data: { first: '123' } };
+        expect(profileReducer(
+            state as ProfileSchema,
+            profileActions.cancelEdit(),
+        )).toEqual({
+            readonly: true, validateErrors: undefined, form: { first: '123' }, data: { first: '123' },
+        });
+    });
+    test('updateProfile', () => {
+        const state: DeepPartial<ProfileSchema> = { form: { first: '333' } };
+        expect(profileReducer(
+            state as ProfileSchema,
+            profileActions.updateProfile({ first: '444' }),
+        )).toEqual({ form: { first: '444' } });
+    });
+    test('test update profile service pending', () => {
+        const state: DeepPartial<ProfileSchema> = {
+            isLoading: false,
+            validateErrors: [ValidateProfileError.SERVER_ERROR],
+        };
+        expect(profileReducer(
+            state as ProfileSchema,
+            updateProfileData.pending,
+        )).toEqual({
+            isLoading: true,
+            validateErrors: undefined,
+        });
+    });
+    test('test update profile service fulfilled', () => {
+        const state: DeepPartial<ProfileSchema> = {
+            isLoading: true,
+        };
+        expect(profileReducer(
+            state as ProfileSchema,
+            updateProfileData.fulfilled(data, ''),
+        )).toEqual({
+            isLoading: false,
+            ValidateProfileError: undefined,
+            readonly: true,
+            form: data,
+            data,
+        });
     });
 });
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Sidebar } from './Sidebar';
 
@@ -14,8 +15,19 @@ export default {
 const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
 
 export const Light = Template.bind({});
+Light.decorators = [StoreDecorator({
+    user: { authData: {} },
+})];
 Light.args = {};
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+    user: { authData: {} },
+})];
+
+export const NoAuth = Template.bind({});
+NoAuth.args = {};
+NoAuth.decorators = [StoreDecorator({
+    user: {},
+})];
