7507f3265a0db61784d8394e6cc2e19cacd9cef8
Igor Bondarenko <igor21v@mail.ru>
2022-12-01 21:27:50 +0800

    Add new theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -6,11 +6,22 @@ interface UseThemeResult {
     theme: Theme;
 }
 
-export function useTheme():UseThemeResult {
+export function useTheme(): UseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
 
     const toggleTheme = () => {
-        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        let newTheme: Theme;
+        switch (theme) {
+        case Theme.DARK:
+            newTheme = Theme.LIGHT;
+            break;
+        case Theme.LIGHT:
+            newTheme = Theme.PURPLE;
+            break;
+        default:
+            newTheme = Theme.DARK;
+            break;
+        }
         setTheme?.(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
