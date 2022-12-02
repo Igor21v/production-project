b0c7714c62b7516ec73ad3806b0c271273f3fdf9
igor21v <igor21v@mail.ru>
2022-12-02 11:45:12 +0300

    update routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,22 +2,24 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
 import { AppRouter } from './providers/router';
 
 export default function App() {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(userActions.initAuthData());
+        console.log('dispatch USERACTION');
     }, [dispatch]);
+    const inited = useSelector(getUserInited);
     return (
         <div className={classNames('app', {}, [])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
-                    <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
