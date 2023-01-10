b4937e8ef52bfc29d18aea04c74f479833362bfc
Igor Bondarenko <igor21v@mail.ru>
2023-01-10 15:17:44 +0800

    Add script for create slice

---
+++ .fttemplates/slice/[FTName]/ui/[FTName]/[FTName].tsx
@@ -0,0 +1,20 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import cls from './<FTName>.module.scss';
+
+interface <FTName>Props {
+    className ?: string;
+}
+
+export const <FTName> = memo((props: <FTName>Props) => {
+    const {
+        className,
+    } = props;
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.<FTName>, {}, [className])} >
+           
+        </div>
+    );
+});
