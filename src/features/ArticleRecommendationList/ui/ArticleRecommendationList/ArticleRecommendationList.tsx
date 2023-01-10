7a28487c690f79b2001ad772844a2a16f8c940f8
Igor Bondarenko <igor21v@mail.ru>
2023-01-10 20:57:46 +0800

    adding RTK query2

--- src/features/ArticleRecommendationList/ui/ArticleRecommendationList/ArticleRecommendationList.tsx
+++ src/features/ArticleRecommendationList/ui/ArticleRecommendationList/ArticleRecommendationList.tsx
@@ -4,17 +4,34 @@ import { memo } from 'react';
 import { VStack } from 'shared/ui/Stack';
 import { ArticleList } from 'entities/Article';
 import { Text, TextSize } from 'shared/ui/Text/Text';
+import { rtkApi } from 'shared/api/rtkApi';
+import { build } from '@reduxjs/toolkit/dist/query/core/buildMiddleware/cacheLifecycle';
 
 interface ArticleRecommendationListProps {
     className ?: string;
 }
+
+const recommendationsApi = rtkApi.injectEndpoints({
+    endpoints: (build) => ({
+        getArticleRecommendationsList: build.query({
+            query: (limit) => ({
+                url: '/articles',
+                params: {
+                    _limit: limit,
+                },
+            }),
+        }),
+    }),
+});
+
+const useArticleRecommendationsList = recommendationsApi.useGetArticleRecommendationsListQuery;
 
 export const ArticleRecommendationList = memo((props: ArticleRecommendationListProps) => {
     const {
         className,
     } = props;
     const { t } = useTranslation('articleDetails');
-
+    const { isLoading, data } = useArticleRecommendationsList(3);
     return (
         <VStack gap="8" className={classNames('', {}, [className])}>
             <Text size={TextSize.L} title={t('We recommend')} />
--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,12 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { Pokemon } from './types';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 export const rtkApi = createApi({
-    reducerPath: 'pokemonApi',
+    reducerPath: 'api',
     baseQuery: fetchBaseQuery({
         baseUrl: __API__,
-        prepareHeaders: (header) => {
+        prepareHeaders: (headers) => {
             const token = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+            if (token) {
+                headers.set('Authorization', token);
+            }
+            return headers;
         },
     }),
     endpoints: () => ({
